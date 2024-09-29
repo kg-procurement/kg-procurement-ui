06ebde4c5aeeb884e22db850d9c7c5816243a863
ferryhieronimus <ferryhieronimus@gmail.com>
2024-09-29 22:55:30 +0700

    [GREEN] implement variant in typography

--- src/components/atoms/typography/index.tsx
+++ src/components/atoms/typography/index.tsx
@@ -1,11 +1,44 @@
 import React from "react";
 
+type VariantKey =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6"
+  | "subtitle1"
+  | "subtitle2"
+  | "body1"
+  | "body2"
+  | "caption";
+
+const variantToTag: Record<VariantKey, string> = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  h5: "h5",
+  h6: "h6",
+  subtitle1: "h6",
+  subtitle2: "h6",
+  body1: "p",
+  body2: "p",
+  caption: "span",
+};
+
 interface TypographyProps {
+  variant?: VariantKey;
   children: React.ReactNode;
 }
 
-const Typography: React.FC<TypographyProps> = ({ children }) => {
-  return <p>{children}</p>;
+const Typography: React.FC<TypographyProps> = ({
+  children,
+  variant = "body2",
+}) => {
+  const Comp = variantToTag[variant];
+
+  return <Comp>{children}</Comp>;
 };
 
 Typography.displayName = "Typography";
