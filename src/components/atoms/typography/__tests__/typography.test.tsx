a73c415a73c6c7f12ef2a5e131606938946aa03d
ferryhieronimus <ferryhieronimus@gmail.com>
2024-09-29 22:53:25 +0700

    [RED] create UT for variant in typography

--- src/components/atoms/typography/__tests__/typography.test.tsx
+++ src/components/atoms/typography/__tests__/typography.test.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import { withWrappers } from "@/lib/testing/utils.tsx";
 import { Typography } from "../index.tsx";
+import "@testing-library/jest-dom";
 
 describe("<Typography />", () => {
   it("should render correctly with correct children", () => {
@@ -8,5 +9,11 @@ describe("<Typography />", () => {
       withWrappers(<Typography>Sample Text</Typography>),
     );
     expect(container.textContent).toMatchInlineSnapshot(`"Sample Text"`);
+  });
+  it('should render as an h1 tag when variant is "h1"', () => {
+    const { container } = render(
+      <Typography variant="h1">Heading 1</Typography>,
+    );
+    expect(container.querySelector("h1")).toBeInTheDocument();
   });
 });
