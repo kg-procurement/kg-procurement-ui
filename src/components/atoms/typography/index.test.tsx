2d66d8ce606a5a7195a302336a9faf6ef9718f75
Hieronimus Ferry <101467366+ferryhieronimus@users.noreply.github.com>
2024-10-01 00:28:08 +0700

    PBI-54: Typography (#2)
    * [RED] create UT for typography component
    
    * [GREEN] implement rendering children in typography
    
    * [RED] create UT for variant in typography
    
    * [GREEN] implement variant in typography
    
    * [RED] create UT for forward ref in typography
    
    * [GREEN] implement forwardRef in typography
    
    * [RED] create UT for className in typography
    
    * [GREEN] implement className in typography
    
    * [REFACTOR] add blank spaces
    
    * [RED] create UT for spreading operators in typography
    
    * [GREEN] implement spreading operators in typography
    
    * [RED] create UT for asChild in typography
    
    * [GREEN] implement asChild in typography
    
    * [REFACTOR] move files
    
    * [REFACTOR] Add proper tsconfig setup, and lint everything according to ESLint
    
    * [REFACTOR] Setup initial sonarqube pipeline
    
    * [REFACTOR] remove unnecesarry case
    
    ---------
    
    Co-authored-by: Andrew Jeremy <andrewjeremy12345@gmail.com>

---
+++ src/components/atoms/typography/index.test.tsx
@@ -0,0 +1,68 @@
+import { render } from '@testing-library/react'
+import { createRef } from 'react'
+
+import { withWrappers } from '@/lib/testing/utils.tsx'
+
+import { Typography } from './index.tsx'
+
+describe('<Typography />', () => {
+  it('should render correctly with correct children', () => {
+    const { container } = render(
+      withWrappers(<Typography>Sample Text</Typography>),
+    )
+    expect(container.textContent).toMatchInlineSnapshot(`"Sample Text"`)
+  })
+
+  it('should render as an h1 tag when variant is "h1"', () => {
+    const { container } = render(
+      <Typography variant="h1">Heading 1</Typography>,
+    )
+    expect(container.querySelector('h1')).toBeInTheDocument()
+  })
+
+  it('should forward ref to the underlying element', () => {
+    const ref = createRef<HTMLParagraphElement>()
+
+    render(<Typography ref={ref}>Sample Text</Typography>)
+
+    expect(ref.current).toBeInstanceOf(HTMLParagraphElement)
+    expect(ref.current?.textContent).toBe('Sample Text')
+  })
+
+  it('should apply the custom className to the element', () => {
+    const { container } = render(
+      <Typography className="custom-class">Sample Text</Typography>,
+    )
+
+    const element = container.querySelector('p')
+    expect(element).toHaveClass('custom-class')
+  })
+
+  it('should apply additional props to the component', () => {
+    const { container } = render(
+      <Typography id="test-id" data-testid="typography-element">
+        Sample Text
+      </Typography>,
+    )
+
+    const element = container.querySelector('p')
+
+    expect(element).toHaveAttribute('id', 'test-id')
+    expect(element).toHaveAttribute('data-testid', 'typography-element')
+  })
+
+  it('should render as a custom element when using asChild', () => {
+    const { container } = render(
+      <Typography asChild>
+        <span data-testid="custom-element">Custom Tag</span>
+      </Typography>,
+    )
+
+    const customElement = container.querySelector(
+      '[data-testid="custom-element"]',
+    )
+
+    expect(customElement).toBeInTheDocument()
+    expect(customElement?.tagName).toBe('SPAN')
+  })
+})
