74045bd65344ebea8a6b71b834a7eb0fad135c4a
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-02 19:42:44 +0700

    [GREEN] add variant for button

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,12 +1,33 @@
+import { cva, type VariantProps } from 'class-variance-authority'
 import * as React from 'react'
 
+import { cn } from '@/utils/cn.ts'
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-400  focus-visible:ring-offset-2 disabled:pointer-events-none',
+  {
+    variants: {
+      variant: {
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+  VariantProps<typeof buttonVariants> {}
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children }, ref) => {
+  ({ className, variant, children }, ref) => {
     return (
-      <button className={className} ref={ref}>
+      <button
+        className={cn(buttonVariants({ variant, className }))}
+        ref={ref}
+      >
         {children}
       </button>
     )
