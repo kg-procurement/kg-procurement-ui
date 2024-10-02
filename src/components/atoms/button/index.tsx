b1643864525d83928698a34ce4aba6ee76daf097
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-02 19:26:26 +0700

    [GREEN] implement displaying children for button

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -1,5 +1,11 @@
-const Button = () => {
-  return <button></button>
+import * as React from 'react'
+
+interface ButtonProps {
+  children?: React.ReactNode
+}
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+  return <button>{children}</button>
 }
 
 Button.displayName = 'Button'
