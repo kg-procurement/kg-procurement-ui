3d21c164b9293a7b4e1588707876a5ed68db2715
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-03 00:09:01 +0700

    [GREEN] add checkbox check

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import { Check } from '@phosphor-icons/react'
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 
 const Checkbox = () => (
   <CheckboxPrimitive.Root>
+    <CheckboxPrimitive.Indicator>
+      <Check className="h-4 w-4" />
+    </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 )
 
