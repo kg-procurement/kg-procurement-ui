b3de44dcce7b4c5b82848a99fa5dda00330dff84
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-03 00:27:52 +0700

    [GREEN] add passing class name implementation

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/utils/cn";
 import { Check } from "@phosphor-icons/react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import React from "react";
@@ -7,9 +8,17 @@ import React from "react";
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({}, ref) => (
-  <CheckboxPrimitive.Root ref={ref}>
-    <CheckboxPrimitive.Indicator>
+>(({className}, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      className,
+    )}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("flex items-center justify-center text-current")}
+    >
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
