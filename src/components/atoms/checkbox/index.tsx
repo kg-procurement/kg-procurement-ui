45cf579b12313c846c1c8a05d16770870d953159
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-02 23:58:17 +0700

    [GREEN] add checkbox implementation

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -3,7 +3,8 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 
 const Checkbox = () => (
-  <></>
+  <CheckboxPrimitive.Root>
+  </CheckboxPrimitive.Root>
 )
 
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
