e2e70ad160a8e061e05e46d4acc8cba62e2e90dd
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-03 00:23:35 +0700

    [GREEN] add checkbox forward ref implementation

--- src/components/atoms/checkbox/index.tsx
+++ src/components/atoms/checkbox/index.tsx
@@ -1,16 +1,20 @@
-'use client'
+"use client";
 
-import { Check } from '@phosphor-icons/react'
-import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import { Check } from "@phosphor-icons/react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import React from "react";
 
-const Checkbox = () => (
-  <CheckboxPrimitive.Root>
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({}, ref) => (
+  <CheckboxPrimitive.Root ref={ref}>
     <CheckboxPrimitive.Indicator>
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-)
+));
 
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox }
+export { Checkbox };
