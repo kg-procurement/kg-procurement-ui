31c9fb3aee6d25787cb18c8ca3c14205cc7a52f1
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-03 00:08:35 +0700

    [RED] add test for check indicator for checkbox

--- src/components/atoms/checkbox/index.test.tsx
+++ src/components/atoms/checkbox/index.test.tsx
@@ -1,7 +1,7 @@
 // Checkbox.test.tsx
 import '@testing-library/jest-dom'
 
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import { Checkbox } from './index.tsx'
 
@@ -11,4 +11,21 @@ describe('<Checkbox />', () => {
     const checkboxElement = screen.getByRole('checkbox')
     expect(checkboxElement).toBeInTheDocument()
   })
+
+  it("should render the indicator when checked", () => {
+    render(<Checkbox />);
+
+    expect(
+      screen.queryByRole("checkbox", { checked: true }),
+    ).not.toBeInTheDocument();
+
+    const checkboxElement = screen.getByRole("checkbox");
+    fireEvent.click(checkboxElement);
+
+    expect(screen.getByRole("checkbox", { checked: true })).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("checkbox").querySelector("svg.h-4.w-4"),
+    ).toBeInTheDocument();
+  });
 })
