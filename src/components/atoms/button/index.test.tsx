3a5b369edc46260d150d2f41332d9bf0c86f344f
Hieronimus Ferry <101467366+ferryhieronimus@users.noreply.github.com>
2024-10-03 23:32:16 +0700

    PBI-57: Button (#5)
    * [RED] add test for button
    
    * [GREEN] implement displaying children for button
    
    * [RED] add test for forwarding ref for  button
    
    * [GREEN] add forwarding ref for button
    
    * [RED] add test for custom className for button
    
    * [GREEN] add custom className for button
    
    * [RED] add test for variant for button
    
    * [GREEN] add variant for button
    
    * [RED] add test for size for button
    
    * [GREEN] add size for button
    
    * [RED] add test for isLoading for button
    
    * [GREEN] add isLoading for button
    
    * [RED] add test for spinner loading for button
    
    * [GREEN] add spinner for button
    
    * [RED] add test for spreading operator for button
    
    * [GREEN] add spreading operator for button
    
    * [GREEN] add onClick test for button

---
+++ src/components/atoms/button/index.test.tsx
@@ -0,0 +1,102 @@
+import { fireEvent, render } from '@testing-library/react'
+import { createRef } from 'react'
+
+import { withWrappers } from '@/lib/testing/utils.tsx'
+
+import { Button } from './index.tsx'
+
+describe('<Button />', () => {
+  it('should render correctly with correct children', () => {
+    const { container } = render(withWrappers(<Button>Sample Text</Button>))
+    expect(container.textContent).toMatchInlineSnapshot(`"Sample Text"`)
+  })
+
+  it('should forward ref to the underlying button element', () => {
+    const ref = createRef<HTMLButtonElement>()
+    render(<Button ref={ref}>Click Me</Button>)
+    expect(ref.current).toBeInstanceOf(HTMLButtonElement)
+    expect(ref.current?.textContent).toBe('Click Me')
+  })
+
+  it('should render with a custom className', () => {
+    const { container } = render(
+      <Button className="custom-button-class">Click Me</Button>,
+    )
+    const buttonElement = container.querySelector('button')
+    expect(buttonElement).toHaveClass('custom-button-class')
+  })
+
+  it('should apply the correct class for the "default" variant', () => {
+    const { container } = render(
+      <Button variant="default">Default Button</Button>,
+    )
+    const buttonElement = container.querySelector('button')
+    expect(buttonElement).toHaveClass(
+      'bg-primary',
+      'text-primary-foreground',
+      'hover:bg-primary/90',
+    )
+  })
+
+  it('should apply the correct class for the "default" size', () => {
+    const { container } = render(
+      <Button size="default">Default Size Button</Button>,
+    )
+    const buttonElement = container.querySelector('button')
+    expect(buttonElement).toHaveClass('h-10', 'py-2', 'px-4')
+  })
+
+  it('should be disabled when isLoading is true', () => {
+    const { getByRole } = render(<Button isLoading>Loading</Button>)
+    const buttonElement = getByRole('button')
+    expect(buttonElement).toBeDisabled()
+  })
+
+  it('should display a spinner when isLoading is true', () => {
+    const { container, getByText } = render(
+      <Button isLoading>Loading...</Button>,
+    )
+
+    const spinner = container.querySelector('svg')
+    expect(spinner).toBeInTheDocument()
+
+    expect(getByText('Loading...')).toBeInTheDocument()
+  })
+
+  it('should apply additional props to the component', () => {
+    const { container } = render(
+      <Button id="test-id" data-testid="button-element">
+        Sample Text
+      </Button>,
+    )
+
+    const element = container.querySelector('button')
+
+    expect(element).toHaveAttribute('id', 'test-id')
+    expect(element).toHaveAttribute('data-testid', 'button-element')
+  })
+
+  it('should call onClick handler when clicked', () => {
+    const handleClick = vitest.fn()
+
+    const { getByRole } = render(
+      <Button onClick={handleClick}>Click Me</Button>,
+    )
+    const buttonElement = getByRole('button')
+    fireEvent.click(buttonElement)
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not call onClick handler when disabled', () => {
+    const handleClick = vitest.fn()
+
+    const { getByRole } = render(
+      <Button isLoading onClick={handleClick}>
+        Click Me
+      </Button>,
+    )
+    const buttonElement = getByRole('button')
+    fireEvent.click(buttonElement)
+    expect(handleClick).not.toHaveBeenCalled()
+  })
+})
