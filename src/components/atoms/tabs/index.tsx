4e10b50041ed3af0dee8275cb239c88a731dfd32
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-03 12:26:17 +0700

    [GREEN] add implementation for forward ref

--- src/components/atoms/tabs/index.tsx
+++ src/components/atoms/tabs/index.tsx
@@ -1,19 +1,42 @@
 "use client";
 
+import * as React from "react";
 import * as TabsPrimitive from "@radix-ui/react-tabs";
+
 
 const Tabs = TabsPrimitive.Root;
 
-const TabsList = ({ ...props }: TabsPrimitive.TabsListProps) => (
-  <TabsPrimitive.List {...props} />
-);
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    {...props}
+  />
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
 
-const TabsTrigger = ({ ...props }: TabsPrimitive.TabsTriggerProps) => (
-  <TabsPrimitive.Trigger {...props} />
-);
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    {...props}
+  />
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
 
-const TabsContent = ({ ...props }: TabsPrimitive.TabsContentProps) => (
-  <TabsPrimitive.Content {...props} />
-);
+const TabsContent = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ ...props }, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    {...props}
+  />
+));
+TabsContent.displayName = TabsPrimitive.Content.displayName;
 
 export { Tabs, TabsList, TabsTrigger, TabsContent };
