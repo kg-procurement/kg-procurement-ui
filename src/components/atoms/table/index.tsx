5b1a949867ac9d10681f9a836a72f613fc5534cc
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-03 23:53:41 +0700

    [GREEN] add implementation forward ref for table

--- src/components/atoms/table/index.tsx
+++ src/components/atoms/table/index.tsx
@@ -1,30 +1,93 @@
-const Table = ({ ...props }) => (
+import * as React from "react";
+
+import { cn } from "@/utils/cn.ts";
+
+const Table = React.forwardRef<
+  HTMLTableElement,
+  React.HTMLAttributes<HTMLTableElement>
+>(({ ...props }, ref) => (
   <div className="relative w-full overflow-auto">
-    <table {...props} />
+    <table
+      ref={ref}
+      {...props}
+    />
   </div>
-);
+));
 
-const TableHeader = ({ ...props }) => <thead {...props} />;
+const TableHeader = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ ...props }, ref) => (
+  <thead ref={ref} {...props} />
+));
 
-const TableBody = ({ ...props }) => <tbody {...props} />;
+const TableBody = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ ...props }, ref) => (
+  <tbody
+    ref={ref}
+    {...props}
+  />
+));
 
-const TableFooter = ({ ...props }) => <tfoot {...props} />;
+const TableFooter = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ ...props }, ref) => (
+  <tfoot
+    ref={ref}
+    {...props}
+  />
+));
 
-const TableRow = ({ ...props }) => <tr {...props} />;
+const TableRow = React.forwardRef<
+  HTMLTableRowElement,
+  React.HTMLAttributes<HTMLTableRowElement>
+>(({ ...props }, ref) => (
+  <tr
+    ref={ref}
+    {...props}
+  />
+));
+TableRow.displayName = "TableRow";
 
-const TableHead = ({ ...props }) => <th {...props} />;
+const TableHead = React.forwardRef<
+  HTMLTableCellElement,
+  React.ThHTMLAttributes<HTMLTableCellElement>
+>(({ ...props }, ref) => (
+  <th
+    ref={ref}
+    {...props}
+  />
+));
+TableHead.displayName = "TableHead";
 
-const TableCell = ({ ...props }) => <td {...props} />;
+const TableCell = React.forwardRef<
+  HTMLTableCellElement,
+  React.TdHTMLAttributes<HTMLTableCellElement>
+>(({ ...props }, ref) => (
+  <td
+    ref={ref}
+    {...props}
+  />
+));
+TableCell.displayName = "TableCell";
 
-const TableCaption = ({ ...props }) => <caption {...props} />;
+const TableCaption = React.forwardRef<
+  HTMLTableCaptionElement,
+  React.HTMLAttributes<HTMLTableCaptionElement>
+>(({ ...props }, ref) => (
+  <caption
+    ref={ref}
+    {...props}
+  />
+));
 
 Table.displayName = "Table";
 TableHeader.displayName = "TableHeader";
 TableBody.displayName = "TableBody";
 TableFooter.displayName = "TableFooter";
-TableRow.displayName = "TableRow";
-TableHead.displayName = "TableHead";
-TableCell.displayName = "TableCell";
 TableCaption.displayName = "TableCaption";
 
 export {
