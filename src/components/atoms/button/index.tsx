913d33c23a3adfedb196ccb5f846a26bba2171bf
Valencius Apriady <v.apriady@gmail.com>
2024-10-04 00:23:30 +0700

    [GREEN] Add implementation for button variant of ghost and outline

--- src/components/atoms/button/index.tsx
+++ src/components/atoms/button/index.tsx
@@ -10,6 +10,9 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        outline:
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
       },
       size: {
         default: 'h-10 py-2 px-4',
