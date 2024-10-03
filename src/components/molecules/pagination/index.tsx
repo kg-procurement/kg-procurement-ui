b99493fd4ac079c9beaceb4d616e8f25b03c3dce
Valencius Apriady <v.apriady@gmail.com>
2024-10-04 01:28:20 +0700

    [RED] Add UT for pagination component

---
+++ src/components/molecules/pagination/index.tsx
@@ -0,0 +1,25 @@
+const Pagination = () => <></>
+const PaginationContent = () => <></>
+const PaginationItem = () => <></>
+const PaginationLink = () => <></>
+const PaginationPrevious = () => <></>
+const PaginationNext = () => <></>
+const PaginationEllipsis = () => <></>
+
+Pagination.displayName = 'Pagination'
+PaginationContent.displayName = 'PaginationContent'
+PaginationItem.displayName = 'PaginationItem'
+PaginationLink.displayName = 'PaginationLink'
+PaginationPrevious.displayName = 'PaginationPrevious'
+PaginationNext.displayName = 'PaginationNext'
+PaginationEllipsis.displayName = 'PaginationEllipsis'
+
+export {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+}
