cd814eefbe6c19b3ae57e838614fb1f31bb66087
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-04 01:48:17 +0700

    [GREEN] add footer implementation

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -1,5 +1,33 @@
+import { Typography } from "@/components/atoms/typography";
+
 const Footer = () => {
-  return ;
+  return (
+    <div className="mt-auto grid h-48 w-full grid-cols-3 gap-6 bg-white p-6">
+      <div className="flex flex-col justify-between">
+        <div>
+          <img
+            src="kompas-gramedia.jpeg"
+            alt="Kompas Gramedia"
+            className="h-16"
+          />
+        </div>
+        <Typography variant="body2" className="ml-8">
+          © 2024 KOMPAS
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="caption" id="footer-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </Typography>
+      </div>
+      <div className="flex justify-end">
+        <Typography variant="subtitle2">Contact us (Email):</Typography>
+      </div>
+    </div>
+  );
 };
 
 Footer.displayName = "Footer";
