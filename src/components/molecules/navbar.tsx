f6b09d912094de4613a1fb4d0033efe18601282b
Valencius Apriady <v.apriady@gmail.com>
2024-10-04 16:20:29 +0700

    [GREEN] Add implementation for Navbar component

--- src/components/molecules/navbar.tsx
+++ src/components/molecules/navbar.tsx
@@ -1,6 +1,18 @@
+import { Typography } from '../atoms/typography/index.tsx'
+
 const Navbar = () => {
   return (
-    <></>
+    <div className="flex justify-between items-center px-5 ">
+      <img
+        src="kompas-gramedia.jpeg"
+        alt="Kompas Gramedia Logo Image"
+        className="w-52"
+      />
+      <div className="flex gap-10">
+        <Typography variant="body1" className="cursor-pointer text-[#828282] hover:text-[#005288] hover:font-bold">Evaluation Form</Typography>
+        <Typography variant="body1" className="cursor-pointer text-[#828282] hover:text-[#005288] hover:font-bold">Search</Typography>
+      </div>
+    </div>
   )
 }
 
