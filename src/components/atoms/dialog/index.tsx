ba48f2ebf21ab31468793eee80942374562078b3
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-04 09:58:42 +0700

    [RED] add tests for dialog

---
+++ src/components/atoms/dialog/index.tsx
@@ -0,0 +1,44 @@
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+
+const Dialog = () => null;
+Dialog.displayName = DialogPrimitive.Root.displayName;
+
+const DialogTrigger = () => null;
+DialogTrigger.displayName = DialogPrimitive.Trigger.displayName;
+
+const DialogPortal = () => null;
+DialogPortal.displayName = DialogPrimitive.Portal.displayName;
+
+const DialogClose = () => null;
+DialogClose.displayName = DialogPrimitive.Close.displayName;
+
+const DialogOverlay = () => null;
+DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
+
+const DialogContent = () => null;
+DialogContent.displayName = DialogPrimitive.Content.displayName;
+
+const DialogHeader = () => null;
+DialogHeader.displayName = "DialogHeader";
+
+const DialogFooter = () => null;
+DialogFooter.displayName = "DialogFooter";
+
+const DialogTitle = () => null;
+DialogTitle.displayName = DialogPrimitive.Title.displayName;
+
+const DialogDescription = () => null;
+DialogDescription.displayName = DialogPrimitive.Description.displayName;
+
+export {
+  Dialog,
+  DialogTrigger,
+  DialogPortal,
+  DialogClose,
+  DialogOverlay,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+};
