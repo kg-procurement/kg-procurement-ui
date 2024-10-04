13d2b804fe599ff48e0cf25793e4f9cc34a2c6b6
ferryhieronimus <ferryhieronimus@gmail.com>
2024-10-04 11:46:08 +0700

    [REFACTOR] increase coverage to 100

---
+++ src/routes/dashboard/__tests__/index.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react'
+
+import { withWrappers } from '@/lib/testing/utils.tsx'
+
+import Page from "../index.tsx";
+
+describe("<Page />", () => {
+  it("should render correctly without crashing", () => {
+    const { container } = render(withWrappers(<Page />));
+    expect(container).toBeInTheDocument();
+  });
+});
