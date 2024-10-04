43a9b2e5aa3e41fafedaf8a4632b363f7b0c7e70
Hieronimus Ferry <101467366+ferryhieronimus@users.noreply.github.com>
2024-10-04 09:19:05 +0700

    PBI-60: Checkbox (#6)
    * [RED] add test for checkbox
    
    * [GREEN] add checkbox implementation
    
    * [RED] add test for check indicator for checkbox
    
    * [GREEN] add checkbox check
    
    * [RED] add test forward ref for checkbox
    
    * [GREEN] add checkbox forward ref implementation
    
    * [RED] add test custom class name for checkbox
    
    * [GREEN] add passing class name implementation
    
    * [RED] add test additional props for checkbox
    
    * [GREEN] add additional props implementation
    
    * [REFACTOR] linting the code
    
    * [REFACTOR] remove unnecessary comment

---
+++ src/components/atoms/checkbox/index.test.tsx
@@ -0,0 +1,53 @@
+import '@testing-library/jest-dom'
+
+import { fireEvent, render, screen } from '@testing-library/react'
+import { createRef } from 'react'
+
+import { Checkbox } from './index.tsx'
+
+describe('<Checkbox />', () => {
+  it('should render the checkbox', () => {
+    render(<Checkbox />)
+    const checkboxElement = screen.getByRole('checkbox')
+    expect(checkboxElement).toBeInTheDocument()
+  })
+
+  it('should render the indicator when checked', () => {
+    render(<Checkbox />)
+
+    expect(
+      screen.queryByRole('checkbox', { checked: true }),
+    ).not.toBeInTheDocument()
+
+    const checkboxElement = screen.getByRole('checkbox')
+    fireEvent.click(checkboxElement)
+
+    expect(screen.getByRole('checkbox', { checked: true })).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('checkbox').querySelector('svg.h-4.w-4'),
+    ).toBeInTheDocument()
+  })
+
+  it('should forward ref to the underlying element', () => {
+    const ref = createRef<HTMLButtonElement>()
+
+    render(<Checkbox ref={ref} />)
+
+    expect(ref.current).toBeInstanceOf(HTMLButtonElement)
+  })
+
+  it('should apply className prop', () => {
+    render(<Checkbox className="custom-class" />)
+    const checkboxElement = screen.getByRole('checkbox')
+    expect(checkboxElement).toHaveClass('custom-class')
+  })
+
+  it('should apply additional props to the component', () => {
+    render(<Checkbox id="test-id" />)
+
+    const element = screen.getByRole('checkbox')
+
+    expect(element).toHaveAttribute('id', 'test-id')
+  })
+})
