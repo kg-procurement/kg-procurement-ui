f96ec089feca371d8d77830c962e1f3e772805db
Hieronimus Ferry <101467366+ferryhieronimus@users.noreply.github.com>
2024-10-04 09:35:34 +0700

    PBI-62: Tabs (#7)
    * [RED] add test for tabs
    
    * [GREEN] add base implementation of tabs
    
    * [RED] add test for forward ref for tabs
    
    * [GREEN] add implementation for forward ref
    
    * [GREEN] add test for checking tab correctness
    
    * [RED] add test for classname for tabs
    
    * [GREEN] add implementation for class name
    
    * [REFACTOR] linting the code

---
+++ src/components/atoms/tabs/index.test.tsx
@@ -0,0 +1,130 @@
+import '@testing-library/jest-dom'
+
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import * as React from 'react'
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './index.tsx'
+
+describe('Tabs Component', () => {
+  it('renders Tabs, TabsList, TabsTrigger, and TabsContent correctly', () => {
+    render(
+      <Tabs defaultValue="tab1">
+        <TabsList>
+          <TabsTrigger value="tab1" id="trigger-tab1">
+            Tab 1
+          </TabsTrigger>
+          <TabsTrigger value="tab2" id="trigger-tab2">
+            Tab 2
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="tab1" id="content-tab1">
+          Content 1
+        </TabsContent>
+        <TabsContent value="tab2" id="content-tab2">
+          Content 2
+        </TabsContent>
+      </Tabs>,
+    )
+
+    expect(screen.getByText('Tab 1')).toBeInTheDocument()
+    expect(screen.getByText('Tab 2')).toBeInTheDocument()
+    expect(screen.getByText('Content 1')).toBeInTheDocument()
+  })
+
+  it('should forward ref to TabsList correctly', () => {
+    const ref = React.createRef<HTMLDivElement>()
+    render(
+      <Tabs defaultValue="tab1">
+        <TabsList ref={ref}>
+          <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+          <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+        </TabsList>
+      </Tabs>,
+    )
+
+    expect(ref.current).toBeInstanceOf(HTMLDivElement)
+  })
+
+  it('should forward ref to TabsTrigger correctly', () => {
+    const ref = React.createRef<HTMLButtonElement>()
+    render(
+      <Tabs defaultValue="tab1">
+        <TabsList>
+          <TabsTrigger ref={ref} value="tab1">
+            Tab 1
+          </TabsTrigger>
+          <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+        </TabsList>
+      </Tabs>,
+    )
+
+    expect(ref.current).toBeInstanceOf(HTMLButtonElement)
+  })
+
+  it('should forward ref to TabsContent correctly', () => {
+    const ref = React.createRef<HTMLDivElement>()
+    render(
+      <Tabs defaultValue="tab1">
+        <TabsList>
+          <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+          <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+        </TabsList>
+        <TabsContent ref={ref} value="tab1">
+          Content 1
+        </TabsContent>
+        <TabsContent value="tab2">Content 2</TabsContent>
+      </Tabs>,
+    )
+
+    expect(ref.current).toBeInstanceOf(HTMLDivElement)
+  })
+
+  it('displays the correct content when a tab is clicked', async () => {
+    render(
+      <Tabs defaultValue="tab1">
+        <TabsList>
+          <TabsTrigger value="tab1" id="trigger-tab1">
+            Tab 1
+          </TabsTrigger>
+          <TabsTrigger value="tab2" id="trigger-tab2">
+            Tab 2
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="tab1" id="content-tab1">
+          Content 1
+        </TabsContent>
+        <TabsContent value="tab2" id="content-tab2">
+          Content 2
+        </TabsContent>
+      </Tabs>,
+    )
+
+    const contentTab2 = document.getElementById('content-tab2')
+
+    // hidden tabs should have the hidden attribute
+    expect(contentTab2).toHaveAttribute('hidden', '')
+
+    // change tab
+    await userEvent.click(screen.getByText('Tab 2'))
+
+    // visible tabs should not have the hidden attribute
+    expect(contentTab2).not.toHaveAttribute('hidden')
+  })
+
+  it('should apply the custom className to TabsList', () => {
+    const { container } = render(
+      <Tabs defaultValue="tab1">
+        <TabsList className="custom-class">
+          <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+          <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+        </TabsList>
+        <TabsContent value="tab1">Content 1</TabsContent>
+        <TabsContent value="tab2">Content 2</TabsContent>
+      </Tabs>,
+    )
+
+    const tabsListElement = container.querySelector('[role="tablist"]')
+    expect(tabsListElement).toHaveClass('custom-class')
+  })
+})
