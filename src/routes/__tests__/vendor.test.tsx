1fb6d173f9ce22cc4888c96d20bec946a2ccf0ea
Valencius Apriady <v.apriady@gmail.com>
2024-10-04 20:37:10 +0700

    [REFACTOR] adjust UT title for vendor page

--- src/routes/__tests__/vendor.test.tsx
+++ src/routes/__tests__/vendor.test.tsx
@@ -4,7 +4,7 @@ import { withWrappers } from '@/lib/testing/utils.tsx'
 
 import Page from '../vendor.tsx'
 
-describe('<Page />', () => {
+describe('<VendorPage />', () => {
   it('should render correctly without crashing', () => {
     render(withWrappers(<Page />))
     expect(screen.getByText('Search Vendor')).toBeInTheDocument()
