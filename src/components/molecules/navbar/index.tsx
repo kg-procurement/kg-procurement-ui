2b52ff6bbdf2fefba2ccc35767aeeadbe55a8fd2
Valencius Apriady <112455892+valenciusap17@users.noreply.github.com>
2024-10-04 17:22:59 +0700

    PBI-73: Navbar (#16)
    * [RED] Add UT for Navbar component
    
    * [GREEN] Add implementation for Navbar component
    
    * [REFACTOR] fix import path to resolve failure on sonar build
    
    * [REFACTOR] adjust folder structure to resolve sonar failure build

---
+++ src/components/molecules/navbar/index.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '../../atoms/typography/index.tsx'
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-between items-center px-5 ">
+      <img
+        src="kompas-gramedia.jpeg"
+        alt="Kompas Gramedia Logo Image"
+        className="w-52"
+      />
+      <div className="flex gap-10">
+        <Typography variant="body1" className="cursor-pointer text-[#828282] hover:text-[#005288] hover:font-bold">Evaluation Form</Typography>
+        <Typography variant="body1" className="cursor-pointer text-[#828282] hover:text-[#005288] hover:font-bold">Search</Typography>
+      </div>
+    </div>
+  )
+}
