c8286d31a69c51185f66a5c4eb70fbc9e66a9ba0
Rahmat Bryan Naufal <87702747+bryannaufal@users.noreply.github.com>
2024-10-05 15:41:26 +0700

    Pbi 17 24 filter vendor UI (#19)
    * [RED] Create UT for filter vendor UI on vendor page
    
    * [GREEN] Create implementation of Filter Vendor UI on vendor page

--- src/components/pages/vendor.tsx
+++ src/components/pages/vendor.tsx
@@ -3,8 +3,14 @@ import PageHeader from '@/components/molecules/page-header.tsx'
 import { Typography } from '../atoms/typography.tsx'
 import VendorTable, { Vendor } from '../features/vendor-table.tsx'
 import { Footer } from '../molecules/footer.tsx'
+import { Input } from '../atoms/input.tsx'
+
+import { useState } from 'react'
 
 const VendorPage = () => {
+  const [productFilter, setProductFilter] = useState('')
+  const [locationFilter, setLocationFilter] = useState('')
+  
   const sampleVendors: Vendor[] = [
     { email: 'a@gmail.com', name: '001', performanceScore: '89' },
     { email: 'a@gmail.com', name: '001', performanceScore: '89' },
@@ -22,6 +28,20 @@ const VendorPage = () => {
         <Typography variant="body1" className="text-white">
           Lorem Ipsum
         </Typography>
+        <div className="flex justify-center gap-4">
+        <Input
+          placeholder="Filter by Product"
+          value={productFilter}
+          onChange={(e) => setProductFilter(e.target.value)}
+          className="w-1/2"
+        />
+        <Input
+          placeholder="Filter by Location"
+          value={locationFilter}
+          onChange={(e) => setLocationFilter(e.target.value)}
+          className="w-1/2"
+        />
+      </div>
       </PageHeader>
       <div className="w-full flex justify-center">
         <VendorTable vendors={sampleVendors} />
