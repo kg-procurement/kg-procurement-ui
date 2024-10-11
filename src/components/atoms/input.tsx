20c96cbd4d2642e10265836b60b347efb41b5402
Rahmat Bryan Naufal <87702747+bryannaufal@users.noreply.github.com>
2024-10-11 17:11:40 +0700

    [GREEN] Complete password visibility toggle implementation in Input component

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Eye, EyeOff } from 'lucide-react'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string
   placeholder?: string
+  isPassword?: boolean
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, placeholder, ...props }, ref) => {
+  ({ className, placeholder, isPassword = false, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
+
     return (
-      <input
-        type="text"
-        ref={ref}
-        className={`rounded border p-2 ${className}`}
-        placeholder={placeholder}
-        {...props}
-      />
+      <div className="relative w-full">
+        <input
+          type={isPassword && showPassword ? 'text' : isPassword ? 'password' : 'text'}
+          ref={ref}
+          className={`rounded border p-2 ${className} ${isPassword ? 'pr-10' : ''}`}
+          placeholder={placeholder}
+          {...props}
+        />
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-2 top-1/2 transform -translate-y-1/2 focus:outline-none"
+          >
+            {showPassword ? <EyeOff className="h-5 w-5" /> : <Eye className="h-5 w-5" />}
+          </button>
+        )}
+      </div>
     )
   },
 )
