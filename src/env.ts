a0f89a396ba88f4d69603ba5ec3b2939c6dd4836
Andrew Jeremy <andrewjeremy12345@gmail.com>
2024-10-11 20:43:01 +0700

    [GREEN] Add handling for import.meta.vitest vs process.env.NODE_ENV === 'test'

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,4 @@
-import { TESTING_URL } from '@/utils/common.ts'
-
-export const API_BASE_URL = import.meta.vitest
-  ? TESTING_URL
-  : import.meta.env.VITE_API_BASE_URL
+export const API_BASE_URL =
+  import.meta.vitest || process.env.NODE_ENV === 'test'
+    ? 'https://www.kg-procurement-testing.com'
+    : import.meta.env.VITE_API_BASE_URL
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,1 @@
 export const noop = () => {}
-
-export const TESTING_URL = 'https://www.kg-procurement-testing.com'
