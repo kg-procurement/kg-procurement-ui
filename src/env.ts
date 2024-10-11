d8b5f3640b285a7f49d32b874a3f0ddfb70f147d
Andrew Jeremy <andrewjeremy12345@gmail.com>
2024-10-11 20:30:53 +0700

    [REFACTOR]: Add vite-plugin-eslint to enforce ESLint rules

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 import { TESTING_URL } from '@/utils/common.ts'
 
-export const API_BASE_URL = process.env.VITEST
+export const API_BASE_URL = import.meta.vitest
   ? TESTING_URL
   : import.meta.env.VITE_API_BASE_URL
