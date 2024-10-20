2da6a4916f8adff49905212cc7f5f0ccae6cf85b
Andrew Jeremy <53634060+FreeJ1nG@users.noreply.github.com>
2024-10-20 15:17:55 +0700

    PBI-109 Edit Vendor Product Integration (#27)
    * [RED] Add barebones for vendor product integration
    
    * [GREEN] Add unit tests for vendor product edit form

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,9 +4,10 @@ import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
 import { setupServer } from 'msw/node'
 
+import { productHandlers } from '@/lib/msw/product.ts'
 import { vendorHandlers } from '@/lib/msw/vendor.ts'
 
-const server = setupServer(...vendorHandlers)
+const server = setupServer(...vendorHandlers, ...productHandlers)
 
 // Start server before all tests
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
