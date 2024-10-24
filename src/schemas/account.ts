c27032fae21a8fad89a2cf01cc0a6ba9ccefe234
edutjie <edutjitrahardja@gmail.com>
2024-10-24 13:28:01 +0700

    [REFACTOR] use interface for Account schema instead of type

--- src/schemas/account.ts
+++ src/schemas/account.ts
@@ -1,11 +1,21 @@
-import { z } from 'zod'
+// TODO: commented for future validation use
+// import { z } from 'zod'
 
-export const accountSchema = z.object({
-  id: z.string(),
-  email: z.string(),
-  password: z.string(),
-  modified_date: z.string().datetime(),
-  created_at: z.string().datetime(),
-})
+// export const accountSchema = z.object({
+//   id: z.string(),
+//   email: z.string(),
+//   password: z.string(),
+//   modified_date: z.string().datetime(),
+//   created_at: z.string().datetime(),
+// })
 
-export type Account = z.infer<typeof accountSchema>
+// export type Account = z.infer<typeof accountSchema>
+
+// TODO: remove this file if the code above is uncommented
+export interface Account {
+  id: string
+  email: string
+  password: string
+  modified_date: string
+  created_at: string
+}
