5e36ebf1b87bf8f5abd31d0c76c1eae526b66b8b
Valencius Apriady <v.apriady@gmail.com>
2024-10-25 14:29:14 +0700

    [REFACTOR] Enhance code readibility

--- src/components/molecules/custom-pagination.tsx
+++ src/components/molecules/custom-pagination.tsx
@@ -22,51 +22,39 @@ function CustomPagination(
   return (
     <Pagination>
       <PaginationContent>
-        {current_page - 1 > 0
-          ? (
-              <PaginationItem>
-                <PaginationPrevious className="cursor-pointer hover:border" onClick={() => setPage(current_page - 1)} />
-              </PaginationItem>
-            )
-          : ''}
-        {current_page - 2 > 0
-          ? (
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            )
-          : ''}
-        {current_page - 1 > 0
-          ? (
-              <PaginationItem>
-                <PaginationLink className="cursor-pointer hover:border">{current_page - 1}</PaginationLink>
-              </PaginationItem>
-            )
-          : ''}
+        {current_page - 1 > 0 && (
+          <PaginationItem>
+            <PaginationPrevious className="cursor-pointer hover:border" onClick={() => setPage(current_page - 1)} />
+          </PaginationItem>
+        )}
+        {current_page - 2 > 0 && (
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+        {current_page - 1 > 0 && (
+          <PaginationItem>
+            <PaginationLink className="cursor-pointer hover:border">{current_page - 1}</PaginationLink>
+          </PaginationItem>
+        )}
         <PaginationItem>
           <PaginationLink isActive>{current_page}</PaginationLink>
         </PaginationItem>
-        {current_page + 1 <= total_page
-          ? (
-              <PaginationItem>
-                <PaginationLink className="cursor-pointer hover:border" onClick={() => setPage(current_page + 1)}>{current_page + 1}</PaginationLink>
-              </PaginationItem>
-            )
-          : ''}
-        {current_page + 2 <= total_page
-          ? (
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            )
-          : ''}
-        {current_page + 1 <= total_page
-          ? (
-              <PaginationItem>
-                <PaginationNext className="cursor-pointer hover:border" onClick={() => setPage(current_page + 1)} />
-              </PaginationItem>
-            )
-          : ''}
+        {current_page + 1 <= total_page && (
+          <PaginationItem>
+            <PaginationLink className="cursor-pointer hover:border" onClick={() => setPage(current_page + 1)}>{current_page + 1}</PaginationLink>
+          </PaginationItem>
+        )}
+        {current_page + 2 <= total_page && (
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+        {current_page + 1 <= total_page && (
+          <PaginationItem>
+            <PaginationNext className="cursor-pointer hover:border" onClick={() => setPage(current_page + 1)} />
+          </PaginationItem>
+        )}
       </PaginationContent>
     </Pagination>
   )
