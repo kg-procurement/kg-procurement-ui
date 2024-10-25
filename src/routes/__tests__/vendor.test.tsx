af1a48e8a99c7d8c7612e943f3598c009a9869a8
Valencius Apriady <v.apriady@gmail.com>
2024-10-25 13:36:33 +0700

    [RED] add unit test code to do email blast

--- src/routes/__tests__/vendor.test.tsx
+++ src/routes/__tests__/vendor.test.tsx
@@ -1,8 +1,24 @@
 import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { http, HttpResponse } from 'msw'
 
+import { API_BASE_URL } from '@/env.ts'
+import { mswServer } from '@/lib/msw/index.ts'
 import { withWrappers } from '@/lib/testing/utils.tsx'
 
 import VendorPage from '../vendor.tsx'
+
+// const { mockBlastEmail } = vi.hoisted(() => ({
+//   mockBlastEmail: vi.fn(),
+// }))
+
+// vi.mock('@/lib/redux/features/vendor/api.ts', () => {
+//   const actual = vi.importActual('@/lib/redux/features/vendor/api.ts')
+//   return {
+//     ...actual,
+//     useBlastEmailMutation: mockBlastEmail,
+//   }
+// })
 
 describe('<VendorPage/>', () => {
   it('should render the header section with the logo', () => {
@@ -39,5 +55,59 @@ describe('<VendorPage/>', () => {
 
     expect(productInput).toBeInTheDocument()
     expect(locationInput).toBeInTheDocument()
+  })
+
+  it('should handle email blast and show success toast when success', async () => {
+    render(withWrappers(<VendorPage />))
+
+    // const blastButton = await waitFor(() => screen.getByRole('button', { name: 'Email Selected Vendor' }))
+    const blastButton = await waitFor(() => screen.getByRole('button', { name: 'Email Selected Vendor' }))
+    await userEvent.click(blastButton)
+
+    // await waitFor(() => {
+    //   const toast = screen.getByTestId('toast')
+    //   expect(toast.innerText).toContain('On Progress')
+    //   expect(toast.innerText).toContain('Executing email blast')
+    // })
+
+    await waitFor(() => {
+      const toast = screen.getByTestId('toast')
+      expect(toast.innerText).includes('Success')
+      expect(toast.innerText).includes('Email blast has successfully executed')
+    })
+
+    // expect(toast).toHaveBeenCalledWith({
+    //   title: 'On Progress',
+    //   description: 'Executing email blast',
+    //   duration: 2000,
+    // })
+
+    // expect(mockToast).toHaveBeenCalledWith({
+    //   title: 'Success',
+    //   description: 'Email blast has successfully executed',
+    //   duration: 2000,
+    // })
+  })
+
+  it('should handle email blast and show error toast when error', async () => {
+    // mockBlastEmail.mockReturnValueOnce(new Error())
+    // http.post(`${API_BASE_URL}/vendor/blast`, () => {
+    //   return HttpResponse.json({ error: [
+    //     '535 5.7.8 Username and Password not accepted. For more information, go to\n5.7.8  https://support.google.com/mail/?p=BadCredentials d2e1a72fcca58-72057931785sm150109b3a.50 - gsmtp',
+    //     '535 5.7.8 Username and Password not accepted. For more information, go to\n5.7.8  https://support.google.com/mail/?p=BadCredentials d2e1a72fcca58-72057939b82sm152223b3a.81 - gsmtp',
+    //   ] }, { status: 207 })
+    // }),
+    mswServer.use(http.post(`${API_BASE_URL}/vendor/blast`, () => HttpResponse.json({}, { status: 500 })))
+
+    render(withWrappers(<VendorPage />))
+
+    const blastButton = await waitFor(() => screen.getByRole('button', { name: 'Email Selected Vendor' }))
+    await userEvent.click(blastButton)
+
+    await waitFor(() => {
+      const toast = screen.getByTestId('toast')
+      expect(toast.innerText).includes('Error')
+      expect(toast.innerText).includes('Email blast failed to be executed')
+    })
   })
 })
