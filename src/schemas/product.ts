63022647715a3d3e6fdc578c257cf4797a38ec88
edutjie <edutjitrahardja@gmail.com>
2024-11-13 18:56:25 +0700

    [RED] adjust product vendors schema

--- src/schemas/product.ts
+++ src/schemas/product.ts
@@ -12,4 +12,31 @@ export const productSchema = z.object({
   modified_by: z.string(),
 })
 
+export const priceSchema = z.object({
+  id: z.string(),
+  price: z.number(),
+  currency_code: z.string(),
+  vendor_id: z.string(),
+  modified_date: z.string().datetime(),
+  modified_by: z.string(),
+})
+
+export const productVendorSchema = z.object({
+  id: z.string(),
+  product: productSchema,
+  price: priceSchema,
+  code: z.string(),
+  name: z.string(),
+  income_tax_id: z.string(),
+  income_tax_name: z.string(),
+  income_tax_percentage: z.string(),
+  description: z.string(),
+  uom_id: z.string(),
+  sap_code: z.string(),
+  modified_date: z.string().datetime(),
+  modified_by: z.string(),
+})
+
 export type Product = z.infer<typeof productSchema>
+export type Price = z.infer<typeof priceSchema>
+export type ProductVendor = z.infer<typeof productVendorSchema>
