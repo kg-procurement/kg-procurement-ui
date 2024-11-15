77a1b0791bb7cbbc08b68d5522b1d955ed251154
Valencius Apriady <v.apriady@gmail.com>
2024-11-15 17:30:40 +0700

    [REFACTOR] add another currency formatting

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,3 +4,8 @@ export const rupiah = (amount: number) => Intl.NumberFormat('id-ID', {
   style: 'currency',
   currency: 'IDR',
 }).format(amount)
+
+export const us_dollar = (amount: number) => Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+}).format(amount)
