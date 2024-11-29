aca064134d3ce08af3514428dcd709151f226a65
ferryhieronimus <ferryhieronimus@gmail.com>
2024-11-29 21:04:51 +0700

    refactor: relocate file

--- src/main.tsx
+++ src/main.tsx
@@ -9,20 +9,7 @@ import store from '@/lib/redux/store.ts'
 import { router } from '@/lib/router.ts'
 
 import { Toaster } from './components/atoms/toaster.tsx'
-import * as Sentry from '@sentry/react'
-
-Sentry.init({
-  dsn: "https://eabf794a73ef0fe9053001dac33190d3@o4508379655372800.ingest.us.sentry.io/4508379657535488",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
-  tracesSampleRate: 1.0,
-  tracePropagationTargets: ["localhost", /https:\/\/.+/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-  enabled: !!import.meta.vitest || process.env.NODE_ENV === "production",
-});
+import "../sentry.config.ts"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
