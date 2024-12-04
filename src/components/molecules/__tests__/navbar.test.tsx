20cab49054aa2cea30acfd07e0b06c089aecddea
Andrew Jeremy <andrewjeremy12345@gmail.com>
2024-12-04 11:58:22 +0700

    [GREEN] Adjust navbar tests to accomodate changes

--- src/components/molecules/__tests__/navbar.test.tsx
+++ src/components/molecules/__tests__/navbar.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
 import { withWrappers } from '@/lib/testing/utils.tsx'
 
@@ -15,12 +14,14 @@ describe('<Navbar />', () => {
   })
   it('should return navigation option', () => {
     render(withWrappers(<Navbar />))
-    expect(screen.getAllByText('Evaluation Form'))
-    expect(screen.getAllByText('Search'))
-  })
-  it('should be able to log out', async () => {
-    // TODO: Adjust this setup when login guard is done
-    render(withWrappers(<Navbar />))
-    await userEvent.click(screen.getByText('Log out'))
+    expect(screen.getAllByTestId('navigation-menu').map(elm => elm.innerText))
+      .toMatchInlineSnapshot(`
+        [
+          "Email Status",
+          "Products",
+          "Vendors",
+          "Evaluation Form",
+        ]
+      `)
   })
 })
